4763527d007d23c507655da11d610c8bb5348535
YamatoCPP <a80442248@gmail.com>
2024-10-16 13:15:39 +0500

    LB6 commit

--- AddEditPage.xaml.cs
+++ AddEditPage.xaml.cs
@@ -13,16 +13,67 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace Gayfullin_Autoservice
 {
     /// <summary>
     /// Логика взаимодействия для AddEditPage.xaml
     /// </summary>
+
     public partial class AddEditPage : Page
     {
-        public AddEditPage()
+        private Service _currentService = new Service();
+        public AddEditPage(Service SelectedService)
         {
             InitializeComponent();
+
+            if (SelectedService != null)
+            {
+                _currentService = SelectedService;
+            }
+            DataContext = _currentService;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            if (string.IsNullOrWhiteSpace(_currentService.Title))
+            {
+                errors.AppendLine("Укажите название услуги");
+            }
+            if (_currentService.Cost <= 0 && !(_currentService.GetType() == typeof(string)) )
+            {
+                errors.AppendLine("Укажите cтоимость услуги");
+            }
+            if (_currentService.Discount < 0 && !(_currentService.GetType() == typeof(string)))
+            {
+                errors.AppendLine("Укажите скидку");
+            }
+            if (string.IsNullOrWhiteSpace(_currentService.Duration))
+            {
+                errors.AppendLine("Укажите длительность услуги");
+            }
+            if (errors.Length> 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+            if (_currentService.Id == 0)
+            {
+                GayfullinAutoServiceEntities.GetContext().Service.Add(_currentService);
+            }
+
+            try
+            {
+                GayfullinAutoServiceEntities.GetContext().SaveChanges();
+                MessageBox.Show("информация сохранена");
+                Manager.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
         }
     }
 }
--- ServicePage.xaml.cs
+++ ServicePage.xaml.cs
@@ -88,5 +88,24 @@ namespace Gayfullin_Autoservice
         {
             UpdateServices();
         }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Service));
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                GayfullinAutoServiceEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                ServiceListView.ItemsSource = GayfullinAutoServiceEntities.GetContext().Service.ToList();
+            }
+        }
     }
 }
